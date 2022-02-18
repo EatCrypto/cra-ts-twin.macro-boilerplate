28e56e05a64247c70c3751c38de7c6ad05704f2f
mingcrypto <mingli970909@gmail.com>
2022-02-18 19:41:44 +1000

    feat: project setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import 'twin.macro'
+
+const App = () => {
+  return (
+    <div tw="relative h-screen">
+      <header tw="absolute top-1/2">Test</header>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: [],
+  plugins: [],
+  theme: {
+    extend: {},
+  },
+}
